d98860b3adcf03d9c1b4a87bcead94c31ad74a00
nelsonfrank <nelsonfr78@outlook.com>
2020-04-05 19:53:08 +0300

    Fix: Replace .css file with .less file #12
    Closes #12

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.css";
+import "./index.less";
 import App from "./app";
 import * as serviceWorker from "./serviceWorker";
 
